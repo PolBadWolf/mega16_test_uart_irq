e860a7c9f3b1bdeef8f0ab342e243f96ecd6f8db
Marat Gumerov <gm.gameoff@gmail.com>
2023-03-13 21:17:49 +0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/*
+ * mega16_test_uart_irq.cpp
+ *
+ * Created: 13.03.2023 21:16:38
+ * Author : User
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
